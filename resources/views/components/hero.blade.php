847c921a894282221ecd06d8db6af58e8527f3d2
tebsss17 <137368444+tebsss17@users.noreply.github.com>
2026-07-28 02:19:24 +0800

    created bg and navbar

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -1,3 +1,22 @@
-<div>
-    <!-- The whole future lies in uncertainty: live immediately. - Seneca -->
+<div class="flex items-center justify-center min-h-screen">
+    <div class="mx-auto max-w-7xl flex items-center gap-12 px-6 py-20 flex-col-reverse lg:flex-row lg:justify-between">
+        {{-- Left --}}
+        <div class="text-center lg:text-left">
+            <p class="font-medium text-xl">Hi, I'm</p>
+
+            <p class="mt-4 font-extrabold text-4xl lg:text-6xl sm:text-3xl">Marc Steven Nagamany</p>
+
+            <p class="mt-4 font-medium text-xl italic underline underline-offset-10">Junior Web Developer</p>
+
+            <p class="mt-6">Turning your IDEAS into SCALABLE web applications</p>
+        </div>
+
+        {{-- Right --}}
+        <div class="bg-blue-200">
+            <img
+                src="/images/profile.jpg"
+                alt="Marc Steven"
+                class="rounded-lg">
+        </div>
+    </div>
 </div>
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,3 +1,21 @@
-<div>
-    <!-- Live as if you were to die tomorrow. Learn as if you were to live forever. - Mahatma Gandhi -->
+<div class="flex items-center justify-center">
+
+    {{-- Desktop Version --}}
+    <nav class="hidden md:flex justify-between items-center w-full border-2 border-white/10 px-10 py-6 rounded-full">
+        <div>
+            <img src="/images/laravel.svg" alt="" class="size-10">
+        </div>
+
+        <div class="flex gap-4 font-medium">
+            <a href="#about">About</a>
+            <a href="#skills">Skills</a>
+            <a href="#projects">Projects</a>
+            <a href="#contact">Contact</a>
+        </div>
+    </nav>
+
+    {{-- Mobile Version --}}
+    <nav class="md:hidden">
+
+    </nav>
 </div>
